ec0a06f7bf89140afab61cd80c91bc5985e8df32
Antonio de las Morenas <a.j.delasmorenas@gmail.com>
2019-11-11 22:20:38 -0600

    Set up server.js and the file structure of the app

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
+
+// If deployed, let Heroku assign a port;
+// otherwise, use port 3000
+const PORT = process.env.PORT || 3000;
+
+// Require all models
+const db = require("./models");
+
+// Initialize Express
+const app = express();
+
+// Make public a static folder
+app.use(express.static("public"));
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// If deployed, use deployed database;
+// otherwise, use local mongoReviews database
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Connect Mongo database to Mongoose
+mongoose.connect(MONGODB_URI);
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }))
+app.set("view engine", "handlebars");
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + ".");
+});
