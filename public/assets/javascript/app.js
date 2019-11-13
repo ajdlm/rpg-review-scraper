ecc56481c941d9a017e08b83d9beed88b7f37e44
Antonio de las Morenas <a.j.delasmorenas@gmail.com>
2019-11-13 17:36:35 -0600

    Added in preventDefault on the search button

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -2,6 +2,8 @@ const db = require("../models/index.js");
 
 $(document).ready(() => {
   $(document).on("click", "#searchButton", () => {
+    event.preventDefault();
+
     db.dropDatabase();
   });
 });
