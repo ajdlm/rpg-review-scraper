722c269450438384004912bec035e1c13fe4fb56
Antonio de las Morenas <a.j.delasmorenas@gmail.com>
2019-11-14 16:28:26 -0600

    Got a lot of the functionality of comments working, although it's still very much unfinished

--- controllers/db_routes.js
+++ controllers/db_routes.js
@@ -5,8 +5,35 @@ const router = express.Router();
 
 const db = require("../models/index.js");
 
+router.get("/api/get-comments/:id", (req, res) => {
+  db.Review.findOne({ _id: req.params.id })
+    .populate("comments")
+    .then(commentedReview => {
+      res.json(commentedReview);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.post("/api/post-comment/:id", (req, res) => {
+  db.Comment.create(req.body)
+    .then(newComment =>
+      db.Review.findOneAndUpdate(
+        { _id: req.params.id },
+        { comments: newComment._id }
+      )
+    )
+    .then(updatedReview => {
+      res.json(updatedReview);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
 router.get("/dropDatabase", function(req, res) {
-    mongoose.connection.db.dropDatabase()
+  mongoose.connection.db.dropDatabase();
 });
 
 module.exports = router;
--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,18 +1,77 @@
 $(document).ready(() => {
-  $(document).on("click", "#dropDbButton", () => {
+  $(document).on("click", "#dropDbButton", event => {
     event.preventDefault();
 
     $.get("/dropDatabase").then(() => {});
   });
 
-  $(document).on("click", ".commentButton", function (event) {
+  $(document).on("click", ".commentButton", function(event) {
     const targetReviewId = $(this).data("id");
 
-    $.get("/findComments").then(commentArray => {
+    $.get("/api/get-comments/" + targetReviewId).then(commentedReview => {
       // Use jQuery to display modal and fill it with the appropriate comments
+
+      console.log(commentedReview);
+
+      $("#commentsHeader").text("Comments for " + commentedReview.reviewTitle);
+
+      $("#makeComment").attr("data-id", targetReviewId);
+
+      for (let i = 0; i < commentedReview.comments.length; i++) {
+        let nextComment = $("<div>");
+
+        let textRow = $("<div>");
+
+        let nextCommentText = $("<p>");
+
+        nextCommentText.text(commentedReview.comments[i].commentText);
+
+        textRow.addClass("col-md-9").append(nextCommentText);
+
+        let buttonRow = $("<div>");
+
+        let nextCommentButton = $("<button>");
+
+        nextCommentButton
+          .text("Delete")
+          .addClass("commentDeleteButton btn btn-danger");
+
+        buttonRow.addClass("col-md-3").append(nextCommentButton);
+
+        nextComment
+          .addClass("row d-flex align-items-center")
+          .append(textRow)
+          .append(buttonRow);
+
+        $("#commentArea").append(nextComment);
+      }
+
+      $("#reviewComments").modal("show");
 
       // Remember to re-empty the div/find and re-append the comments again
       // when the user adds one of their own
     });
+  });
+
+  $(document).on("click", "#makeComment", function(event) {
+    event.preventDefault();
+
+    const toBeCommented = $(this).data("id");
+
+    const newUsername = $("#username")
+      .val()
+      .trim();
+
+    const newInput = $("#commentInput")
+      .val()
+      .trim();
+
+    const userComment = {
+      commenter: newUsername,
+
+      commentText: newInput
+    };
+
+    $.post("/api/post-comment/" + toBeCommented, userComment, (req, res) => {});
   });
 });
